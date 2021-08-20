e7e02206af56f9cf242c0d27fc45ab3cbb9ea0e3
Brian Burrous <brianon007@gmail.com>
2021-08-20 08:01:07 -0700

    Added joystick.ino

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -25,34 +25,35 @@ void loop() {
   yVal = 1023 - yVal;
   yVal -= 512;
   xVal -= 512;
+  Serial.print("X value: " + String(xVal)+"\n");
+  Serial.print("Y value: " + String(yVal)+"\n");
+
+
 
 
   // Check if joystick is at origin
-  if (xVal == 0 && yVal == 0) {
+  if (abs(xVal) <= 25 && abs(yVal) <= 25) {
     outputString = "Origin";
+    Serial.print(outputString+"\n");
   }
   else {
-    xDir =  (xVal < 0) ? "left" : "right"; //determine the sign of x and y componenents
-    yDir = (yVal < 0) ? "down" : "up";
-
-    //if x and y components equal magnitude, concatentate them
-    if (abs(xVal) == abs(yVal)) {
-      outputString = xDir + " " + yDir;
-    } 
+    xDir =  (xVal < -25) ? "left" : "right"; //determine the sign of x and y componenents
+    yDir = (yVal < -25) ? "down" : "up";
     
-    //Otherwise, determine which component is more significant
-    else {
       outputString = (abs(xVal) > abs(yVal)) ? xDir : yDir;
+      Serial.print(outputString+"\n");
     }
-  }
+  delay(1000);
+}
+/*
 
   //Output string now contains the correct direction
-  Serial.print(outputString);
+  //Serial.print(outputString);
 
-  /*int r = sqrt(xVal*xVal + yVal*yVal);
+  int r = sqrt(xVal*xVal + yVal*yVal);
 
   int theta = atan2(yVal, xVal);
   if (yVal < 0){
     theta = 360 - theta;
-  }*/
-}
+  }
+}*/
