c3dcfba17269abb7b123386674ec1d752b8f41e5
Brian Burrous <brianon007@gmail.com>
2021-08-20 17:09:58 -0700

    function documentation

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -1,4 +1,4 @@
-#include<math.h>
+#include <math.h>
 
 #define X A0
 #define Y A1
@@ -14,9 +14,10 @@ String inY;
 
 int getPolarRadius(int, int);
 double getPolarAngle(int, int);
+string getDirection(int, int);
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   pinMode(X, INPUT);
   pinMode(Y, INPUT);
@@ -24,61 +25,54 @@ void setup() {
   Serial.setTimeout(10000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  xVal = analogRead(X);
-  yVal = analogRead(Y);
+void loop()
+{
+  
+  xRaw = analogRead(X);
+  yRaw = analogRead(Y);
 
-  yVal = 1023 - yVal;
-  yVal -= 512;
-  xVal -= 512;
-
-
-  // Check if joystick is at origin
-  if (abs(xVal) <= 25 && abs(yVal) <= 25) {
-    outputString = "Origin";
-  }
-  else {
-    xDir =  (xVal < -25) ? "left" : "right"; //determine the sign of x and y componenents
-    yDir = (yVal < -25) ? "down" : "up";
-    outputString = (abs(xVal) > abs(yVal)) ? xDir : yDir;
-    }
-  // Serial.println(outputString);
-  //delay(5000);
-  Serial.println("Enter x coordinate: ");
-  inX = Serial.readStringUntil('\n');
-  Serial.println("Enter y coordinate: ");
-  inY = Serial.readStringUntil('\n');
-  Serial.println("inX: " + inX);
-  Serial.println("inY: " + inY);
-  Serial.print(inX.toInt());
-
-  xVal = inX.toInt();
-  yVal = inY.toInt();
-  //Output string now contains the correct direction
-  //Serial.print(outputString);
+  // Remap 
+  xVal = map(xRaw, 0, 1023, -512, 512);
+  //Y input range increases as it goes down, so range must be flipped
+  yVal = map(yRaw, 1023, 0, -512, 512);
 
   int r = getPolarRadius(xVal, yVal);
   double theta = getPolarAngle(xVal, yVal);
-  Serial.println("R: "+String(r));
-  Serial.println("Theta: " + String(theta));
-
-
 }
 
 
-
-int getPolarRadius(int x, int y) {
-  return sqrt(x*x + y*y);
+int getPolarRadius(int x, int y)
+{
+  return sqrt(x * x + y * y);
 }
 
-
-
-double getPolarAngle(int x, int y) {
+//Returns polar angle in degrees, from 0 to 360
+double getPolarAngle(int x, int y)
+{
   double theta = atan2(y, x);
-  theta = 180/3.14*theta;
-  if (theta < 0){
+  theta = 180 / 3.14 * theta;
+  if (theta < 0)
+  {
     theta = 360 + theta;
   }
   return theta;
+}
+
+//Returns string of direction of input. 
+string getDirection(int xVal, int yVal)
+{
+  const int center = 25; //Range of volatage magnitudes that should register as no input (origin)
+  string outputString;
+  // Check if joystick is at origin
+  if (abs(xVal) <= center && abs(yVal) <= center)
+  {
+    outputString = "Origin";
+  }
+  else
+  {
+    xDir = (xVal < -center) ? "left" : "right"; //determine the sign of x and y componenents
+    yDir = (yVal < -center) ? "down" : "up";
+    outputString = (abs(xVal) > abs(yVal)) ? xDir : yDir;
+  }
+  return outputString;
 }
